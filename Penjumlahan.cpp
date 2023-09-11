f98b86c64f0317226f7aaa46a1061dfea76a96e0
Banuuu1 <144661623+Banuuu1@users.noreply.github.com>
2023-09-11 14:39:19 +0700

    Create Penjumlahan.cpp

---
+++ Penjumlahan.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int a, b, c;
+    cout<< "Program Penjumlahan \n";
+     cout<<"Masukkan Angka pertama: ";
+     cin>>a;
+     cout<<"Masukan Angka kedua: ";
+     cin>>b;
+     c=a+b;
+     cout<<"Hasilnya adalah: "<<c<<endl;
+     return 0;
+}
